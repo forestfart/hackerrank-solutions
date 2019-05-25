abaac2be0395462fbf93982116b32cad7c8c0d54
Michal Michalik <michal.michalik@gmail.com>
2019-05-25 18:16:26 +0200

    streams fun

---
+++ src/main/java/oracle/ocp/Streams.java
@@ -0,0 +1,32 @@
+package oracle.ocp;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Streams {
+    public static void main(String[] args) {
+        List<String> lst = new ArrayList<>();
+        lst.add("a");
+        lst.add("ab");
+        lst.stream().forEach((s) -> System.out.println(s));
+        lst.stream().forEach(System.out::println);
+        lst.stream().forEach((k) -> k.length());
+    }
+}
+
+class Streams2 {
+    public static void main(String[] args) {
+        Stream<Integer> stream = Stream.of(4, 40, 8, 3);
+        System.out.println(stream.peek(System.out::print).anyMatch(x -> x > 10));
+    }
+}
+class Streams3 {
+    public static void main(String[] args) {
+        Stream list = Stream.of("ABC", "aBB", "AbCC", "ab", "acC");
+        Object map = list.collect(Collectors.partitioningBy((String s) -> s.startsWith("ab")));
+        System.out.println(map);
+    }
+
+}
