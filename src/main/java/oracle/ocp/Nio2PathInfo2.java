55c0835db997e0ee5fa00ad9e90448b7bfd7d8fa
Michal Michalik <michal.michalik@gmail.com>
2019-06-15 12:45:51 +0200

    nio2 test

--- src/main/java/oracle/ocp/Nio2PathInfo2.java
+++ src/main/java/oracle/ocp/Nio2PathInfo2.java
@@ -28,3 +28,21 @@ class Nio2PathInfo2 {
 
 }
 
+class PathCompare1 {
+    public static void main(String[] args) {
+        Path path1 = Paths.get("Test");
+        Path path2 = Paths.get("D:\\OCPJP\\programs\\NIO2\\Test");
+        // comparing two paths using compareTo() method
+        System.out.println("(path1.compareTo(path2) == 0) is: "
+                + (path1.compareTo(path2) == 0));
+
+        // comparing two paths using equals() method
+        System.out.println("path1.equals(path2) is: " + path1.equals(path2));
+
+        // comparing two paths using equals() method with absolute path
+        System.out.println("path2.equals(path1.toAbsolutePath()) is "
+                + path2.equals(path1.toAbsolutePath()));
+    }
+}
+
+
