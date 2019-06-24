04938197521a12ba0088c366c2a749e3816822fa
Michal Michalik <michal.michalik@gmail.com>
2019-06-24 12:57:24 +0200

    the basic lambda

--- src/main/java/oracle/ocp/Lambda.java
+++ src/main/java/oracle/ocp/Lambda.java
@@ -5,9 +5,17 @@ interface LambdaFunction {
     void call(String t);
 }
 
+@FunctionalInterface
+interface StrangeInterfaceCheck {
+    void tellMeArea(int a, int b);
+}
+
 public class Lambda {
     public static void main(String[] args) {
         LambdaFunction lambdaFunction = System.out::println;
         lambdaFunction.call("the Message");
+
+        StrangeInterfaceCheck nice = (a, b) -> System.out.println(a * b);
+        nice.tellMeArea(10,10);
     }
 }
