3ffb9b57d95a94b1cb4971f4733028c1d11a4289
Michal Michalik <michal.michalik@gmail.com>
2019-08-05 11:25:19 +0200

    locales

---
+++ src/main/java/oracle/ocp/locale/TalkativeResourceBundleControl.java
@@ -0,0 +1,34 @@
+package oracle.ocp.locale;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+class TalkativeResourceBundleControl extends ResourceBundle.Control {
+    // override the default getCandidateLocales method to print
+    // the candidate locales first
+    public List<Locale> getCandidateLocales(String baseName, Locale locale) {
+        List<Locale> candidateLocales = super.getCandidateLocales(baseName, locale);
+        System.out.printf("Candidate locales for base bundle name %s and locale %s %n", baseName, locale.getDisplayName());
+        candidateLocales.forEach(System.out::println);
+        return candidateLocales;
+    }
+}
+
+class CandidateLocales {
+    public static void loadResourceBundle(String resourceBundleName, Locale locale) {
+        ResourceBundle resourceBundle = ResourceBundle.getBundle(resourceBundleName, locale, new TalkativeResourceBundleControl());
+        String rbLocaleName = resourceBundle.getLocale().toString();
+        if (rbLocaleName.equals("")) {
+            System.out.println("Loaded the default property file with name: "
+                    + resourceBundleName);
+        } else {
+            System.out.println("Loaded the resource bundle for the locale: "
+                    + resourceBundleName + "." + rbLocaleName);
+        }
+    }
+
+    public static void main(String[] args) {
+        loadResourceBundle("ResourceBundle", new Locale("pl", "PLZ", ""));
+    }
+}
