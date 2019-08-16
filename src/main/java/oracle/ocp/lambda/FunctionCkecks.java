32dc1cf22ecd2df821c2b99a29f0b4ad5b4cabb9
Michal Michalik <michal.michalik@gmail.com>
2019-08-16 20:49:17 +0200

    lambda compose andThen tests

---
+++ src/main/java/oracle/ocp/lambda/FunctionCkecks.java
@@ -0,0 +1,13 @@
+package oracle.ocp.lambda;
+
+import java.util.function.Function;
+
+public class FunctionCkecks {
+
+    public static void main(String args[]) {
+        Function<Integer, String> fun = s -> s.toString();
+        Function<String, String> fun2 = fun.andThen((String s) -> s + "2")
+                .compose((String s) -> Integer.parseInt(s));
+    }
+
+}
