8a7581992b4d0988fdc9b4d2c14ec2ee7fc28c25
Michal Michalik <michal.michalik@gmail.com>
2019-08-17 14:56:24 +0200

    nio walk file tree

---
+++ src/main/java/oracle/ocp/nio/Mat.java
@@ -0,0 +1,37 @@
+package oracle.ocp.nio;
+
+import java.io.IOException;
+import java.io.InvalidObjectException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.PathMatcher;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.regex.Pattern;
+
+public class Mat {
+    static boolean f;
+
+    public static void main(String args[]) throws IOException {
+        Path path = Paths.get("C:\\Users\\HP\\Music");
+        FileVisitor<Path> searcher = new Search();
+        Path s = Files.walkFileTree(path, searcher);
+        if (!f) System.out.println("No matches");
+    }
+
+    private static final class Search extends SimpleFileVisitor<Path> {
+        public FileVisitResult visitFile(Path file, BasicFileAttributes basicFileAttributes) throws IOException {
+            PathMatcher pm = FileSystems.getDefault().getPathMatcher("glob:*.*");
+            if (pm.matches(file.getFileName())) {
+                System.out.println(file);
+                f = true;
+            }
+            return FileVisitResult.CONTINUE;
+        }
+    }
+}
