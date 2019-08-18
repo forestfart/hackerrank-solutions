f298ee1908f50ba6a8abb3910bc7738058e8e336
Michal Michalik <michal.michalik@gmail.com>
2019-08-18 19:51:20 +0200

    regex harness test

---
+++ src/main/java/oracle/ocp/regex/RegexTestHarness.java
@@ -0,0 +1,36 @@
+package oracle.ocp.regex;
+
+import java.io.BufferedReader;
+import java.io.Console;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexTestHarness {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+
+            System.out.print("Enter your regex: ");
+            Pattern pattern = Pattern.compile(console.readLine());
+            System.out.print("Enter input string to search: ");
+            Matcher matcher = pattern.matcher(console.readLine());
+
+            boolean found = false;
+            while (matcher.find()) {
+                System.out.printf("I found the text" +
+                                " \"%s\" starting at " +
+                                "index %d and ending at index %d.%n",
+                        matcher.group(),
+                        matcher.start(),
+                        matcher.end());
+                found = true;
+            }
+            if(!found){
+                System.out.printf("No match found.%n");
+            }
+        }
+    }
+}
