08fee2e90696bee4fa1cf8bbeabe106c6ab76604
Michal Michalik <michal.michalik@gmail.com>
2019-08-19 15:59:05 +0200

    yep.. heaps of fun...

--- src/main/java/oracle/ocp/time/ChronoUnitsCheck.java
+++ src/main/java/oracle/ocp/time/ChronoUnitsCheck.java
@@ -1,8 +1,11 @@
 package oracle.ocp.time;
 
-import java.time.LocalDate;
+import java.time.Duration;
 import java.time.LocalDateTime;
+import java.time.Period;
 import java.time.temporal.ChronoUnit;
+
+import static java.time.Duration.between;
 
 public class ChronoUnitsCheck {
 
@@ -11,7 +14,10 @@ public class ChronoUnitsCheck {
         LocalDateTime localDate2 = LocalDateTime.of(2019,10,29, 0, 0);
 
         long l = localDate1.until(localDate2, ChronoUnit.HALF_DAYS);
-
         System.out.println(l);
+
+        Duration duration = between(localDate1, localDate2);
+        System.out.printf("Duration between %s and %s is %s %s", localDate1, localDate2, duration, duration.getUnits());
+
     }
 }
