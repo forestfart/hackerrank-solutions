1fc1af2cfa5129d02c72f73d9cb4334e02c0c7e3
Michal Michalik <michal.michalik@gmail.com>
2019-09-01 16:19:22 +0200

    int supplier test

---
+++ src/main/java/oracle/ocp/stream/IntSupplierTest.java
@@ -0,0 +1,15 @@
+package oracle.ocp.stream;
+
+import java.util.function.IntSupplier;
+
+public class IntSupplierTest {
+
+    public static void main(String[] args) {
+        IntSupplier ins = IntSupplierTest::getRandomInt10;
+        System.out.println(ins.getAsInt());
+    }
+
+    static int getRandomInt10() {
+        return (int)(Math.random()*10)+1;
+    }
+}
