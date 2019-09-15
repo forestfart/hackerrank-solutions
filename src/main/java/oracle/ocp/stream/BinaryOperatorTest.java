f564209b9b2556499399d590ce0a5245f603467c
Michal Michalik <michal.michalik@gmail.com>
2019-09-15 11:32:47 +0200

    fun wirt int binary operator...

--- src/main/java/oracle/ocp/stream/BinaryOperatorTest.java
+++ src/main/java/oracle/ocp/stream/BinaryOperatorTest.java
@@ -5,7 +5,15 @@ import java.util.function.IntBinaryOperator;
 public class BinaryOperatorTest {
     public static void main(String[] args) {
         IntBinaryOperator inp = (i1, i2) -> i1 / i2;
-        System.out.println(inp.applyAsInt(53,5));
+        System.out.println(inp.applyAsInt(53, 5));
+    }
+}
+
+class BinOperator2 {
+    public static void main(String[] args) {
+        IntBinaryOperator ibo = Integer::compare;
+        System.out.println(ibo.applyAsInt(10,12));
     }
 
 }
+
