ae0284db1ab7e281f31c9f69c77223b0b92c0739
Michal Michalik <michal.michalik@gmail.com>
2019-09-16 22:11:37 +0200

    fun with concurrency'ssssss...

--- src/main/java/oracle/ocp/concurrent/AwaitTerminationTest.java
+++ src/main/java/oracle/ocp/concurrent/AwaitTerminationTest.java
@@ -3,8 +3,11 @@ package oracle.ocp.concurrent;
 import java.time.Duration;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
@@ -19,16 +22,18 @@ public class AwaitTerminationTest {
         }
     }
 
-    static class Task2 implements Runnable {
+    static class Task2 implements Callable<String> {
 
         @Override
-        public void run() {
+        public String call() {
             System.out.println("Task #2");
             try {
                 Thread.sleep(4500);
             } catch (InterruptedException e) {
                 long elapsed = Duration.between(start, Instant.now()).toMillis();
-                System.err.printf("Interrupted exception arrived after: %sms", elapsed);;
+                System.err.printf("Interrupted exception arrived after: %sms", elapsed);
+            } finally {
+                return "Passed the variable ;>";
             }
         }
     }
@@ -37,11 +42,17 @@ public class AwaitTerminationTest {
 
         ExecutorService pool = Executors.newFixedThreadPool(2);
 
-        pool.submit(new Task1());
-        pool.submit(new Task2());
+        Future future1 = pool.submit(new Task1());
+        Future future2 = pool.submit(new Task2());
 
         pool.awaitTermination(4, TimeUnit.SECONDS);
 
         pool.shutdownNow();
+
+        try {
+            System.out.printf("\nFutures: #1: %s #2: %s", future1.get(), future2.get());
+        } catch (ExecutionException e) {
+            System.err.println("Unable to retrieve futures");
+        }
     }
 }
