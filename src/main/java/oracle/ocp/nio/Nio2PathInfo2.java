8aa70e2568c7564428f756900fdbf7c88386a647
Michal Michalik <michal.michalik@gmail.com>
2019-09-29 21:43:24 +0200

    #8 fun..

--- src/main/java/oracle/ocp/nio/Nio2PathInfo2.java
+++ src/main/java/oracle/ocp/nio/Nio2PathInfo2.java
@@ -60,4 +60,16 @@ class PathCheck1 {
     }
 }
 
+class PathCheck2 {
+    public static void main(String[] args) {
+        Path path1 = Paths.get(".\\app\\.\\sys\\");
+        Path res1 = path1.resolve("log");
+        Path path2 = Paths.get("\\server\\exe\\");
+        Path res2 = path2.resolve("\\readme.exe");
+        System.out.println(res1);
+        System.out.println(res2);
+
+    }
+}
+
 
