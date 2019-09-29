8ffbf5c861080d8a0e8846423ccf40d5e55f6f96
Michal Michalik <michal.michalik@gmail.com>
2019-09-29 23:15:42 +0200

    #9 fun..

---
+++ src/main/java/oracle/ocp/locale/PropertiesClazz.java
@@ -0,0 +1,38 @@
+package oracle.ocp.locale;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class PropertiesClazz {
+    public static void main(String[] args) throws IOException {
+        Properties prop = new Properties();
+        String userName = "dfs", password = "pass";
+        prop.put("user", userName); prop.put("password", password);
+        System.out.println(prop.getProperty("user" + " " + prop.getProperty("password")));
+        try {
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost/high_performance_java_persistence", prop);
+            Statement statement = con.createStatement();
+            String query = "SELECT * FROM Users";
+            ResultSet resultSet = statement.executeQuery(query);
+            resultSet.first();
+            while (resultSet.next()) {
+                String userNamed = resultSet.getString(1);
+                System.out.println(userNamed);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        FileInputStream fis = new FileInputStream(".\\src\\main\\resources\\ResourceBundle.properties");
+        prop.load(fis);
+        System.out.println(prop.getProperty("Greeting"));
+        System.out.println(prop.getProperty("Greeting2","Test")); //line n1
+        System.out.println(prop.getProperty("welcome3"));
+    }
+}
