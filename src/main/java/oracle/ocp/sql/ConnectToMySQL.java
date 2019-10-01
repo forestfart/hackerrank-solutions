086cf3ed088eeca9f99d0166b1442945cc74bf32
Michal Michalik <michal.michalik@gmail.com>
2019-10-01 23:17:20 +0200

    database fun'ssssss...

--- src/main/java/oracle/ocp/sql/ConnectToMySQL.java
+++ src/main/java/oracle/ocp/sql/ConnectToMySQL.java
@@ -7,16 +7,34 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class ConnectToMySQL {
+    static Connection newConnection;
+
     public static void main(String[] args) throws SQLException {
         try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/library_kodilla?serverTimezone=Europe/Warsaw&useSSL=False", "library_user", "my_password")) {
-            Statement statement = connection.createStatement();
+            connection.setAutoCommit(false);
+
+            Statement statement;
             String query = "SELECT * from Book";
-            ResultSet resultSet = statement.executeQuery(query);
-            printToConsole(resultSet);
+            printToConsole(connection, query);
+
+            statement = connection.createStatement();
+            String delete = "DELETE from Book WHERE book_id = 98";
+            System.out.println(" Delete Status: " + statement.executeUpdate(delete));
+            printToConsole(connection, query); // updates table no matter if it is autocommit true or false!
+
+            statement = connection.createStatement();
+            String insert = "INSERT INTO Book VALUES('98', 'anonymous', '2016', 'OCPPractice')";
+            System.out.println(" Insert Status: " + statement.executeUpdate(insert));
+            printToConsole(connection, query);
+
+            newConnection = connection;
         }
+        // newConnection.createStatement(); //throws SQLNonTransientConnectionException
     }
 
-    private static void printToConsole(ResultSet resultSet) throws SQLException {
+    private static void printToConsole(Connection connection, String query) throws SQLException {
+        ResultSet resultSet = connection.createStatement().executeQuery(query);
+        resultSet.absolute(1);
         while (resultSet.next()) {
             System.out.print(resultSet.getString(1) + "\t" + resultSet.getString(2) + "\t" + resultSet.getString(3) + "\t" + resultSet.getString(4) + "\n");
         }
