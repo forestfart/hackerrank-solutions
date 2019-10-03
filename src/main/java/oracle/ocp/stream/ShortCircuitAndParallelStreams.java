7d0bc9c1258baba08e75f4167438aa99dfc17fa3
Michal Michalik <michal.michalik@gmail.com>
2019-10-03 22:04:59 +0200

    short circut but tricky one'ssssss...

--- src/main/java/oracle/ocp/stream/ShortCircuitAndParallelStreams.java
+++ src/main/java/oracle/ocp/stream/ShortCircuitAndParallelStreams.java
@@ -22,11 +22,11 @@ class AllMatchFun {
     public static void main(String[] args) {
         List<String> colors = Arrays.asList("red", "green", "yellow");
         Predicate<String> test = n -> {
-            System.out.println("Searching…");
+            System.out.println("Searching…(" + n + ")");
             return n.contains("red");
         };
-        colors.stream()
+        System.out.println(colors.stream()
                 .filter(c -> c.length() > 3)
-                .allMatch(test);
+                .allMatch(test));
     }
 }
