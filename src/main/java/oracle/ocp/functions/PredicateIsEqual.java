ecf17bcd3b7b7901b35278562dd08f879ef4c60b
Michal Michalik <michal.michalik@gmail.com>
2019-10-03 21:25:22 +0200

    predicate by fun'ssssss...

--- src/main/java/oracle/ocp/functions/PredicateIsEqual.java
+++ src/main/java/oracle/ocp/functions/PredicateIsEqual.java
@@ -9,5 +9,11 @@ public class PredicateIsEqual {
         Predicate<String> ins = Predicate.isEqual(in);
         System.out.println(ins.test("2"));
     }
+}
 
+class PredicateRefactor {
+    public static void main(String[] args) {
+        Predicate<String> res = n -> n.length() > 5 && n.length() < 10;
+        res.test("Hello");
+    }
 }
