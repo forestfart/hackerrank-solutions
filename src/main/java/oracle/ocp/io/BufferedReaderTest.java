b7c06354775bf8db1614ebeedf37c5abaea914e7
Michal Michalik <michal.michalik@gmail.com>
2019-10-06 14:52:48 +0200

    buffered stream mark'ssssss...

--- src/main/java/oracle/ocp/io/BufferedReaderTest.java
+++ src/main/java/oracle/ocp/io/BufferedReaderTest.java
@@ -3,10 +3,12 @@ package oracle.ocp.io;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -40,5 +42,23 @@ class WriteFiles {
             bufferedWriter.append(line + "\n");
         }
         bufferedWriter.flush();
+    }
+}
+
+class AutoCloseableFun {
+    public static void main(String[] args) {
+        try (FileInputStream fis = new FileInputStream("src\\main\\resources\\BufferedReaderInput.txt");
+             InputStreamReader isr = new InputStreamReader(fis);
+             BufferedReader bufferedReader = new BufferedReader(isr)) {
+            if (bufferedReader.markSupported()) {
+                System.out.println((char) bufferedReader.read());
+                bufferedReader.mark(12);
+                System.out.println((char) bufferedReader.read());
+                bufferedReader.reset();
+                System.out.println((char) bufferedReader.read());
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        }
     }
 }
