bce4036e261a0f0875522d93ff8a516100a43e1d
Michal Michalik <michal.michalik@gmail.com>
2019-10-07 22:09:47 +0200

    optional'ssssss...

--- src/main/java/oracle/ocp/io/BufferedReaderTest.java
+++ src/main/java/oracle/ocp/io/BufferedReaderTest.java
@@ -62,3 +62,11 @@ class AutoCloseableFun {
         }
     }
 }
+
+class BufferedReaderInputFun {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+        int number = Integer.parseInt(br.readLine());
+        System.out.println(number);
+    }
+}
--- src/main/java/oracle/ocp/optional/OfNullableTest.java
+++ src/main/java/oracle/ocp/optional/OfNullableTest.java
@@ -3,6 +3,35 @@ package oracle.ocp.optional;
 import java.util.Optional;
 import java.util.stream.Stream;
 
+class Employee {
+    Optional<Address> address;
+    Employee (Optional<Address> address) {
+        this.address = address;
+    }
+    public Optional<Address> getAddress() {
+        return address;
+    }
+}
+
+class Address {
+    String city = "New York";
+    public String getCity() {
+        return city;
+    }
+
+    public String toString() {
+        return city;
+    }
+
+    public static void main(String[] args) {
+        Address address = null;
+        Optional<Address> address1 = Optional.ofNullable(address);
+        Employee e1 = new Employee(address1);
+        System.out.println((address1.isPresent()) ? address1.get().getCity() : "City Not available");
+    }
+}
+
+
 public class OfNullableTest {
 
     public static void main(String[] args) {
@@ -10,7 +39,7 @@ public class OfNullableTest {
 
         Optional<String> optionalString = stream.findAny();
 
-        optionalString.ofNullable(null).ifPresent(System.out::println); // <<- xD brain damage!
+        optionalString.ofNullable(null).ifPresent(System.out::println); // <<- xD brain damage! does not trigger Sys.out, static member accessed via instance reference
         Optional.ofNullable(optionalString).ifPresent(System.out::println);
 
         System.out.println(optionalString);
