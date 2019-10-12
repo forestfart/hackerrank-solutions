d8676d96a4bd3087968c92a8860f47c4f35ebdf4
Michal Michalik <michal.michalik@gmail.com>
2019-10-12 22:49:58 +0200

    files concurrent fun..

--- src/main/java/oracle/ocp/concurrent/ExecutorTest.java
+++ src/main/java/oracle/ocp/concurrent/ExecutorTest.java
@@ -1,12 +1,18 @@
 package oracle.ocp.concurrent;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 class Task implements Runnable {
     public void run() {
@@ -66,3 +72,23 @@ class ExecutorsTestThree {
         es.shutdown();
     }
 }
+
+class FileThread implements Runnable {
+    String fName;
+    public FileThread(String fName) {
+        this.fName = fName;
+    }
+    public void run() {
+        System.out.println(fName);
+    }
+    public static void main (String[] args) throws IOException, InterruptedException {
+        ExecutorService executor = Executors.newCachedThreadPool();
+        Stream<Path> listOfFiles = Files.walk(Paths.get("src\\main\\resources"));
+        listOfFiles.forEach(line -> {
+            executor.execute(new FileThread(line.getFileName().toString())); // line n1
+        });
+        executor.shutdown();
+        executor.awaitTermination(5, TimeUnit.DAYS); // line n2
+    }
+}
+
