f95d1fe77ee5721036730d8cae677fabcc50c780
Michal Michalik <michal.michalik@gmail.com>
2019-10-13 00:27:13 +0200

    io fun..

--- src/main/java/oracle/ocp/functions/SupplierFun.java
+++ src/main/java/oracle/ocp/functions/SupplierFun.java
@@ -4,12 +4,13 @@ import java.util.function.Supplier;
 
 public class SupplierFun {
     static class Bird {
-        public void fly () {
+        public void fly() {
             System.out.print("Can fly");
         }
     }
+
     static class Penguin extends Bird {
-        public void fly () {
+        public void fly() {
             System.out.print("Cannot fly");
         }
     }
@@ -18,7 +19,16 @@ public class SupplierFun {
         fly(() -> new SupplierFun.Bird());
         fly(Penguin::new);
         fliesToo(Bird::new);
+
+        try {
+            fly(Bird::new);
+        } catch (ArithmeticException | NumberFormatException e) {
+            System.out.println(e.getMessage());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
+
     static void fly(Supplier<Bird> bird) {
         bird.get().fly();
     }
