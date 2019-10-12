498f1c178ccd6d6b8124f3108b0147d010fc3607
Michal Michalik <michal.michalik@gmail.com>
2019-10-12 22:21:02 +0200

    fork join action fun..

--- src/main/java/oracle/ocp/stream/StreamOfStreams.java
+++ src/main/java/oracle/ocp/stream/StreamOfStreams.java
@@ -6,7 +6,9 @@ import java.util.stream.Stream;
 
 public class StreamOfStreams {
     public static void main(String[] args) {
-        Stream<List<String>> iStr = Stream.of(Arrays.asList("1", "John"), Arrays.asList("2", null));
+        Stream<List<String>> iStr = Stream.of(
+                Arrays.asList("1", "John"),
+                Arrays.asList("2", null));
         Stream<String> nInSt = iStr.flatMap(x -> x.stream());
         nInSt.forEach(System.out::print);
     }
