bc5796d6d96359ae5e1262628c326c02e737cb75
Michal Michalik <michal.michalik@gmail.com>
2019-10-13 00:12:29 +0200

    supplier fun..

---
+++ src/main/java/oracle/ocp/functions/SupplierFun.java
@@ -0,0 +1,29 @@
+package oracle.ocp.functions;
+
+import java.util.function.Supplier;
+
+public class SupplierFun {
+    static class Bird {
+        public void fly () {
+            System.out.print("Can fly");
+        }
+    }
+    static class Penguin extends Bird {
+        public void fly () {
+            System.out.print("Cannot fly");
+        }
+    }
+
+    public static void main(String[] args) {
+        fly(() -> new SupplierFun.Bird());
+        fly(Penguin::new);
+        fliesToo(Bird::new);
+    }
+    static void fly(Supplier<Bird> bird) {
+        bird.get().fly();
+    }
+
+    static void fliesToo(Supplier<? extends Bird> bird) {
+        bird.get().fly();
+    }
+}
