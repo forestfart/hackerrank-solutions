49a2b59b83fa40fb9e3a38281f8608fa70e5d905
Michal Michalik <michal.michalik@gmail.com>
2019-10-13 01:23:10 +0200

    interface fun..

--- src/main/java/oracle/ocp/clazz/InterfaceFun.java
+++ src/main/java/oracle/ocp/clazz/InterfaceFun.java
@@ -10,5 +10,27 @@ public interface InterfaceFun {
     static boolean equalsNOOOO(Object o) {
         return true;
     };
+}
 
+interface Doable {
+    public void doSomething(String s);
+}
+abstract class Task implements Doable {
+    public void doSometingElse(String s) {}
+}
+/*abstract class Work implements Doable {
+    public abstract void doSomething(String s){};
+    public void doYourThing(Boolean b) {}
+}
+class Job implements Doable {
+    public void doSomething(Integer i) {}
+}
+class Action implements Doable {
+    public void doSomething(Integer i) {}
+    public String doThis(Integer j) {}
+}*/
+class Do implements Doable {
+    public void doSomething(Integer i) {}
+    public void doSomething(String s) {}
+    public void doThat(String s) {}
 }
