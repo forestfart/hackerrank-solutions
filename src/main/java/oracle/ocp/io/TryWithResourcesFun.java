07e37b2b71df7cbc1b5cb8360a4f1952c2c2f8e1
Michal Michalik <michal.michalik@gmail.com>
2019-10-15 10:31:50 +0200

    hopefully last batch of fun'ssssss...

--- src/main/java/oracle/ocp/io/TryWithResourcesFun.java
+++ src/main/java/oracle/ocp/io/TryWithResourcesFun.java
@@ -60,5 +60,36 @@ class ImagePrinter implements AutoCloseable {
     }
 }
 
+class Resource implements AutoCloseable {
+
+    public void close() throws Exception {
+        System.out.println("Close-");
+    }
+
+    public void open(){
+        System.out.println("Open-");
+    }
+}
+
+class funFun {
+    public static void main(String[] args) {
+        // and given code fragment:
+        Resource res1 = new Resource();
+
+        try {
+            res1.open();
+            res1.close();
+        } catch (Exception e) {
+            System.out.println("Exception - 1");
+        }
+
+        try (Resource res2 = new Resource()) {	// line n1
+            res1.open();
+        } catch (Exception e) {
+            System.out.println("Exception - 2");
+        }
+    }
+
+}
 
 
