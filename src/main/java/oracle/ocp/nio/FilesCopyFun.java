6798e41e64f151882edb0284b0093964fde5c64f
Michal Michalik <michal.michalik@gmail.com>
2019-10-16 14:02:23 +0200

    done!

--- src/main/java/oracle/ocp/nio/FilesCopyFun.java
+++ src/main/java/oracle/ocp/nio/FilesCopyFun.java
@@ -7,7 +7,7 @@ import java.nio.file.Paths;
 
 public class FilesCopyFun {
     public static void main(String[] args) throws IOException {
-        Path source = Paths.get("src\\main\\java\\oracle\\ocp\\nio\\green.txt");
+        Path source = Paths.get("src\\main\\java\\oracle\\ocp\\nio\\gr33n.txt"); // <- throws NoSuchFileException if it is not found.. damn it!
         Path destination = Paths.get("src\\main\\resources\\green.txt");
         Files.copy(source,destination);
     }
