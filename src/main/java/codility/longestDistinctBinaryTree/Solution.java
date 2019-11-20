4c70527a6bc319a7671f67643ac8fb5529078f71
Michal Michalik <michal.michalik@gmail.com>
2019-11-20 11:45:22 +0100

    some codility recursive solution

---
+++ src/main/java/codility/longestDistinctBinaryTree/Solution.java
@@ -0,0 +1,36 @@
+package codility.longestDistinctBinaryTree;
+
+import java.util.*;
+
+class Tree {
+    public int x;
+    public Tree l;
+    public Tree r;
+}
+
+public class Solution {
+    private static int max = 0;
+
+    public static void main(String[] tree) {
+        Solution solution = new Solution();
+        solution.solution(new Tree());
+    }
+
+    public int solution(Tree tree) {
+        // write your code in Java SE 8
+        return traverse(tree, new TreeSet<>());
+    }
+
+    private static int traverse(Tree node, Set<Integer> trace) {
+        if (trace.add(node.x)) {
+            if (max < trace.size()) max = trace.size();
+            if (node.l != null) {
+                traverse(node.l, new TreeSet<>(trace));
+            }
+            if (node.r != null) {
+                traverse(node.r, new TreeSet<>(trace));
+            }
+        }
+        return max;
+    }
+}
