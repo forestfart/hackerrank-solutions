823c08c2b10cce7efcd4789c535f51a014c75d4c
forestm <michal.michalik@gmai.com>
2021-03-03 11:46:38 +0100

    Arrays left rotation

--- src/main/java/practice/interview/prepraration/kit/arrays/leftRotation/LeftRotationSolution.java
+++ src/main/java/practice/interview/prepraration/kit/arrays/leftRotation/LeftRotationSolution.java
@@ -1,4 +1,14 @@
 package practice.interview.prepraration.kit.arrays.leftRotation;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class LeftRotationSolution {
+    // Complete the rotLeft function below.
+    static int[] rotLeft(int[] a, int d) {
+        int[] arr1 = Arrays.copyOfRange(a, 0, d);
+        int[] arr2 = Arrays.copyOfRange(a, d, a.length);
+        return IntStream.concat(Arrays.stream(arr2), Arrays.stream(arr1)).toArray();
+    }
+
 }
