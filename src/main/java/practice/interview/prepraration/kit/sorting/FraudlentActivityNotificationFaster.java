c77602ac77bf55325ef9e9fbbfa1dfe12033e3e9
forestm <michal.michalik@gmai.com>
2021-03-08 12:34:39 +0100

    Most efficient

--- src/main/java/practice/interview/prepraration/kit/sorting/FraudlentActivityNotificationFaster.java
+++ src/main/java/practice/interview/prepraration/kit/sorting/FraudlentActivityNotificationFaster.java
@@ -1,48 +1,58 @@
 package practice.interview.prepraration.kit.sorting;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class FraudlentActivityNotificationFaster {
 
     // Complete the activityNotifications function below.
     static int activityNotifications(int[] expenditure, int d) {
 
-        int size;
+        int check1, check2;
+        double average;
+        int elementToRemove;
         int elementToAdd;
         int notifications = 0;
-        List<Integer> trailingList = new ArrayList<>();
+        List<Integer> trailingList = new ArrayList<>(d);
+        int insertIndex;
 
         if (expenditure.length <= d) return 0;
 
-        for (int i = 1; i < expenditure.length; i++) {
+        check2 = d / 2;
+        if (d % 2 != 0) {
+            check1 = check2;
+        } else {
+            check1 = (d / 2) - 1;
+        }
+
+        for (int i = 1; i < d; i++) {
+            elementToAdd = expenditure[i - 1];
+            insertIndex = Collections.binarySearch(trailingList, elementToAdd);
+            if (insertIndex < 0) {
+                insertIndex = (-1 * insertIndex) - 1;
+            }
+            trailingList.add(insertIndex, elementToAdd);
+        }
+
+        System.out.println(trailingList.size() + " " + d);
+
+        for (int i = d; i < expenditure.length; i++) {
+
             if (trailingList.size() == d) {
-                int elementToRemove = expenditure[i - d - 1];
-                System.out.println("remove: " + elementToRemove);
+                elementToRemove = expenditure[i - d - 1];
                 trailingList.remove((Integer) elementToRemove);
             }
             elementToAdd = expenditure[i - 1];
-            trailingList.add(elementToAdd);
-            System.out.println("add: " + elementToAdd);
-
-            trailingList = trailingList.stream()
-                    .sorted()
-                    .collect(Collectors.toList());
-            System.out.println(trailingList.toString());
-
-            double average;
-            size = trailingList.size();
-            if (size % 2 != 0) {
-                average = trailingList.get(size / 2) * 2;
-            } else {
-                average = (trailingList.get((size / 2) - 1) + trailingList.get(size / 2));
+            insertIndex = Collections.binarySearch(trailingList, elementToAdd);
+            if (insertIndex < 0) {
+                insertIndex = (-1 * insertIndex) - 1;
             }
+            trailingList.add(insertIndex, elementToAdd);
 
-            if (expenditure[i] >= average && trailingList.size() == d) {
-                notifications++;
+            if (trailingList.size() == d) {
+                average = trailingList.get(check1) + trailingList.get(check2);
+                if (expenditure[i] >= average) notifications++;
             }
         }
         return notifications;
     }
-
 }
---
+++ src/main/java/practice/interview/prepraration/kit/sorting/FraudlentActivityNotificationFastest.java
@@ -0,0 +1,49 @@
+package practice.interview.prepraration.kit.sorting;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class FraudlentActivityNotificationFastest {
+
+    // Complete the activityNotifications function below.
+    static int activityNotifications(int[] expenditure, int d) {
+        int count = 0;
+        double median = 0;
+        int tempIndex = d - 1;
+        boolean odd = (d % 2 == 0) ? false : true;
+        int index = odd ? d / 2 : d / 2 - 1;
+        List<Integer> temp = IntStream.of(expenditure)
+                .limit(d)
+                .boxed()
+                .sorted()
+                .collect(Collectors.toList());
+        Function<Integer, Double> fn = i -> odd ?
+                (double) temp.get(i) :
+                (double) (temp.get(i) + temp.get(i + 1)) / 2;
+
+        for (int i = d; i < expenditure.length; i++) {
+            median = fn.apply(index);
+
+            if (expenditure[i] >= median * 2) {
+                count++;
+            }
+            if (expenditure[i] == expenditure[i - d]) {
+                continue;
+            }
+
+            int rmIndex = Collections.binarySearch(temp, expenditure[i - d]);
+            temp.remove(rmIndex);
+            tempIndex = Collections.binarySearch(temp, expenditure[i]);
+            if (tempIndex < 0) {
+                tempIndex = (-1 * tempIndex) - 1;
+            }
+            temp.add(tempIndex, expenditure[i]);
+        }
+
+        return count;
+    }
+
+}
