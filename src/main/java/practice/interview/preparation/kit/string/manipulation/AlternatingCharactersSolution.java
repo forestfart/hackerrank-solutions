bcc28aab647b85648a88c1e392f58dfd592af2af
forestm <michal.michalik@gmai.com>
2021-03-16 09:10:55 +0100

    simple worm up

--- src/main/java/practice/interview/preparation/kit/string/manipulation/AlternatingCharactersSolution.java
+++ src/main/java/practice/interview/preparation/kit/string/manipulation/AlternatingCharactersSolution.java
@@ -3,10 +3,10 @@ package practice.interview.preparation.kit.string.manipulation;
 public class AlternatingCharactersSolution {
     // Complete the alternatingCharacters function below.
     static int alternatingCharacters(String s) {
-        int count = 0;
-        for (int  i=1; i < s.length(); i++) {
-            int n = i;
-            while (n < s.length() && s.charAt(i - 1) == s.charAt(n)) {
+        int count = 0, n, length = s.length();
+        for (int i = 1; i < length; i++) {
+            n = i;
+            while (n < length && s.charAt(i - 1) == s.charAt(n)) {
                 count++;
                 n++;
             }
